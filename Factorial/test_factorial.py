b4e57ca25f8431005c01707d7376601a7730e62b
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-05-08 13:30:40 -0300

    Prueba de factorial negativo

--- Factorial/test_factorial.py
+++ Factorial/test_factorial.py
@@ -8,3 +8,9 @@ class TestFactorial(unittest.TestCase):
         self.assertEqual(factorial_iterative(1), 1)
         self.assertEqual(factorial_iterative(5), 120)
         self.assertEqual(factorial_iterative(10), 3628800)
+
+    def test_factorial_iterative_negative_numbers(self):
+        with self.assertRaises(ValueError):
+            factorial_iterative(-1)
+        with self.assertRaises(ValueError):
+            factorial_iterative(-5)
